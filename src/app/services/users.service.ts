35d9faa612a059bc6224dd09030bf44365cee47f
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-04-16 21:12:37 -0300

    Feat: creating the user services and register method

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+import { LoginResponse } from '../types/login-response.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  apiUrl: string = 'http://localhost:8080/user';
+
+  constructor(private httpClient: HttpClient) { }
+
+  signup(name: string, login: string, password: string) {
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, login, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth_token", value.token);
+      })
+    )
+  }
+}
