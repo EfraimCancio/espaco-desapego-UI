21e4466d75948ec7c45e68aeb47d8d41cf1e353d
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-05-22 11:01:46 -0300

    Feat: Aplication configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
+import { authInteceptorInterceptor } from './security/auth-inteceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +14,11 @@ export const appConfig: ApplicationConfig = {
     provideToastr(),
     provideClientHydration(),
     provideAnimations(),
-    provideHttpClient(withFetch())
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        authInteceptorInterceptor
+      ])
+    ),
   ]
 };
