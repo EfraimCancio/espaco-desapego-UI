35be2057ede7914f62cb73e2a474c821d2dca8ff
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-05-22 10:58:48 -0300

    Feat: Creating the product view

--- src/app/components/products-components/proucts-table/proucts-table.component.ts
+++ src/app/components/products-components/proucts-table/proucts-table.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ProductsService } from '../../../services/products.service';
 
 @Component({
   selector: 'app-proucts-table',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './proucts-table.component.html',
   styleUrl: './proucts-table.component.css'
 })
+
 export class ProuctsTableComponent {
 
+  constructor(
+    private productsService: ProductsService,
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.listActiveProducts();
+  }
+
+  listActiveProducts = () => {
+    console.log('listActiveProducts! ')
+
+    this.productsService.getActiveProducts().subscribe({
+      next: res => {
+        console.log('Lista de produtos ativos', res);
+      },
+      error: err => {
+        console.log('Erro na requisição', err);
+      }
+    })
+  };
 }
