08a4282a259ed45ef7dcea670f4161e378e32cd8
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-05-23 20:03:58 -0300

    Feat: Creating the category service

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  apiUrl: string = 'http://localhost:8080/categorias';
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  getActiveProducts() {
+    const url = this.apiUrl + '/ativos'
+    return this.httpClient.get(url)
+  }
+}
