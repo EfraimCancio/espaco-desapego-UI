8fbbef36459277431d2214408aebcdb8c0173d02
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-05-23 18:42:37 -0300

    Feat: Configuring the options choises in the dashboard menu

--- src/app/components/dashboard-menu/dashboard-menu.component.ts
+++ src/app/components/dashboard-menu/dashboard-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-menu',
@@ -7,6 +7,48 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard-menu.component.html',
   styleUrl: './dashboard-menu.component.css'
 })
+
 export class DashboardMenuComponent {
 
+  @Input() showTable: string = "";
+  @Output() onClickEvent = new EventEmitter<string>();
+
+  activeChoice: string = 'products';
+
+  onClick = (table: string) => {
+    this.activeChoice = table;
+    this.showTable = table;
+    this.onClickEvent.emit(this.showTable);
+    console.log('PAssou o evento: ', this.showTable);
+  };
+
+  // activeProducts = () => {
+  //   this.activeChoice = 'products';
+  //   this.showTable = 'products';
+  // }
+
+  // activeCategory = () => {
+  //   this.activeChoice = 'categories';
+  //   this.showTable = 'categories';
+  // };
+
+  // activeBrands = () => {
+  //   this.activeChoice = 'brands';
+  //   this.showTable = 'brands';
+  // };
+
+  // activeSizes = () => {
+  //   this.activeChoice = 'sizes';
+  //   this.showTable = 'sizes';
+  // };
+
+  // activeOrigins = () => {
+  //   this.activeChoice = 'origins';
+  //   this.showTable = 'origins';
+  // };
+
+  // activeUsers = () => {
+  //   this.activeChoice = 'users';
+  //   this.showTable = 'users';
+  // };
 }
