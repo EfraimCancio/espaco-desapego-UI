a9f4b2c759ef3bddd074a7ce0b986424a086358b
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-05-23 19:34:29 -0300

    Feat: Create product registration form

---
+++ src/app/components/products-components/product-registration/product-registration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-registration',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-registration.component.html',
+  styleUrl: './product-registration.component.css'
+})
+export class ProductRegistrationComponent {
+
+}
