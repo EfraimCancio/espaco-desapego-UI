1c0bebe54cdd9a2776b279e38a4ccb5bbb08c0c3
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-05-23 20:22:23 -0300

    Feat: Implementing the categories table

--- src/app/components/categories-components/categories-table/categories-table.component.ts
+++ src/app/components/categories-components/categories-table/categories-table.component.ts
@@ -1,12 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { CategoryService } from '../../../services/category.service';
 
 @Component({
   selector: 'app-categories-table',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TableModule
+  ],
   templateUrl: './categories-table.component.html',
   styleUrl: './categories-table.component.css'
 })
+
 export class CategoriesTableComponent {
 
+  arrayActiveCategory: any;
+
+  constructor(
+    private categoriesService: CategoryService
+  ) { }
+
+  ngOnInit(): void {
+    this.listActiveCategories();
+  }
+
+  listActiveCategories = () => {
+    console.log('listar Categoras! ')
+
+    this.categoriesService.getActiveCategories().subscribe({
+      next: res => {
+        this.arrayActiveCategory = res
+        console.log('Lista de categorias ativas', this.arrayActiveCategory);
+      },
+      error: err => {
+        console.log('Erro na requisição', err);
+      }
+    })
+  };
+
+  registerCategory = () => {
+    console.log('Registrar Categorias');
+  };
 }
