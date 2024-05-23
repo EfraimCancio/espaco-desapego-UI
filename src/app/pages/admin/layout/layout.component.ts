49d84d35b5956b03c10dcbe8c8fc791535135e65
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-05-23 19:24:45 -0300

    Feat: creating the table display logic in the layout component

--- src/app/pages/admin/layout/layout.component.ts
+++ src/app/pages/admin/layout/layout.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
+import { BrandsTableComponent } from "../../../components/brands-components/brands-table/brands-table.component";
+import { CategoriesTableComponent } from "../../../components/categories-components/categories-table/categories-table.component";
 import { DashboardMenuComponent } from "../../../components/dashboard-menu/dashboard-menu.component";
 import { NavbarComponent } from "../../../components/navbar/navbar.component";
+import { OriginsTableComponent } from "../../../components/origins-components/origins-table/origins-table.component";
 import { ProuctsTableComponent } from "../../../components/products-components/proucts-table/proucts-table.component";
+import { SizesTableComponent } from "../../../components/sizes-components/sizes-table/sizes-table.component";
+import { UsersTableComponent } from "../../../components/users-components/users-table/users-table.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
-  imports: [DashboardMenuComponent, NavbarComponent, ProuctsTableComponent]
+  imports: [DashboardMenuComponent, NavbarComponent, ProuctsTableComponent, CategoriesTableComponent, BrandsTableComponent, SizesTableComponent, OriginsTableComponent, UsersTableComponent]
 })
 export class LayoutComponent {
   tableChoice: string = '';
   onChildEvent(event: string) {
-    console.log('Evento recebido:', event);
+    this.tableChoice = event
+    console.log('Evento recebido:', this.tableChoice);
   }
 }
