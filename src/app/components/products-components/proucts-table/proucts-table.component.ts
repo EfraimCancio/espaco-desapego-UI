25058e5950968034fe6901b5375e519e7a2348ed
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-05-23 10:48:34 -0300

    Feat: Implementing the prime NG Table

--- src/app/components/products-components/proucts-table/proucts-table.component.ts
+++ src/app/components/products-components/proucts-table/proucts-table.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
 import { ProductsService } from '../../../services/products.service';
 
 @Component({
   selector: 'app-proucts-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TableModule
+  ],
   templateUrl: './proucts-table.component.html',
   styleUrl: './proucts-table.component.css'
 })
@@ -14,48 +18,6 @@ import { ProductsService } from '../../../services/products.service';
 export class ProuctsTableComponent {
 
   arrayActiveProducts: any;
-
-  lista: any = [
-    {
-      "id": 6,
-      "descProduto": "camisa teste",
-      "idCategoria": 1,
-      "idMarca": 2,
-      "idTamanho": null,
-      "idOrigem": 5,
-      "vlCusto": 70.0,
-      "vlVenda": 80.0,
-      "dtEntrada": "2024-02-16",
-      "dtSaida": null,
-      "status": true
-    },
-    {
-      "id": 8,
-      "descProduto": "Teste response",
-      "idCategoria": 1,
-      "idMarca": 2,
-      "idTamanho": 4,
-      "idOrigem": 5,
-      "vlCusto": 70.0,
-      "vlVenda": null,
-      "dtEntrada": "2024-03-14",
-      "dtSaida": null,
-      "status": true
-    },
-    {
-      "id": 9,
-      "descProduto": "Teste Final",
-      "idCategoria": 2,
-      "idMarca": 3,
-      "idTamanho": 6,
-      "idOrigem": 5,
-      "vlCusto": 70.0,
-      "vlVenda": null,
-      "dtEntrada": "2024-03-25",
-      "dtSaida": null,
-      "status": true
-    }
-  ];
 
   constructor(
     private productsService: ProductsService,
