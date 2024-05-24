a492917df2b8773ff649f0514c0a2c751759fb7d
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-05-24 10:48:17 -0300

    Feat: Creating users table

--- src/app/components/users-components/users-table/users-table.component.ts
+++ src/app/components/users-components/users-table/users-table.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { UsersService } from '../../../services/users.service';
 
 @Component({
   selector: 'app-users-table',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TableModule
+  ],
   templateUrl: './users-table.component.html',
   styleUrl: './users-table.component.css'
 })
+
 export class UsersTableComponent {
 
+  arrayUsers: any;
+
+  constructor(
+    private userService: UsersService
+  ) { };
+
+  ngOnInit(): void {
+    this.listUsers();
+  }
+
+  listUsers = () => {
+    console.log('listar Categoras! ')
+    this.userService.getAllUsers().subscribe({
+      next: res => {
+        this.arrayUsers = res
+        console.log('Lista de Usuarios: ', this.arrayUsers);
+      },
+      error: err => {
+        console.log('Erro na requisição', err);
+      }
+    })
+  };
+
+  registerUser = () => {
+    console.log('registrar usuario');
+  };
 }
