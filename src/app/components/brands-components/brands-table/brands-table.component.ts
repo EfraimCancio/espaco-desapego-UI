fb33c2e2bac847031dc7c281ecd72673ef8bcf32
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-05-24 09:24:41 -0300

    Feat: Creating brands table

--- src/app/components/brands-components/brands-table/brands-table.component.ts
+++ src/app/components/brands-components/brands-table/brands-table.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { BrandsService } from '../../../services/brands.service';
 
 @Component({
   selector: 'app-brands-table',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TableModule
+  ],
   templateUrl: './brands-table.component.html',
   styleUrl: './brands-table.component.css'
 })
+
 export class BrandsTableComponent {
 
+  arrayActiveBrands: any;
+
+  constructor(
+    private brandsService: BrandsService,
+  ) { };
+
+  ngOnInit(): void {
+    this.listActiveCategories();
+  }
+
+  listActiveCategories = () => {
+    console.log('listar Categoras! ')
+    this.brandsService.getActiveBrands().subscribe({
+      next: res => {
+        this.arrayActiveBrands = res
+        console.log('Lista de categorias ativas', this.arrayActiveBrands);
+      },
+      error: err => {
+        console.log('Erro na requisição', err);
+      }
+    })
+  };
+
+  registerBrand = () => {
+    console.log('Lista de Marcas', this.arrayActiveBrands)
+  };
 }
