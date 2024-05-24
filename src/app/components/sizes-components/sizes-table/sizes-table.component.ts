c97414ae5350f9ecd695925f1dc7c990bd4f2e60
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-05-24 09:44:58 -0300

    Feat: Creating sizes table

--- src/app/components/sizes-components/sizes-table/sizes-table.component.ts
+++ src/app/components/sizes-components/sizes-table/sizes-table.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { SizesService } from '../../../services/sizes.service';
 
 @Component({
   selector: 'app-sizes-table',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TableModule
+  ],
   templateUrl: './sizes-table.component.html',
   styleUrl: './sizes-table.component.css'
 })
 export class SizesTableComponent {
+  arrayActiveSizes: any;
 
+  constructor(
+    private sisesService: SizesService,
+  ) { };
+
+  ngOnInit(): void {
+    this.listActiveSizes();
+  }
+
+  listActiveSizes = () => {
+    console.log('listar Categoras! ')
+    this.sisesService.getActiveSizes().subscribe({
+      next: res => {
+        this.arrayActiveSizes = res
+        console.log('Lista de tamanhos ativas', this.arrayActiveSizes
+        );
+      },
+      error: err => {
+        console.log('Erro na requisição', err);
+      }
+    })
+  };
+
+  registerSize = () => {
+    console.log('Lista de Sizes: ', this.arrayActiveSizes)
+  };
 }
